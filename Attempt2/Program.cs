7bcffcdea9c94b4b9cac8c071322f1df4d67dadb
Mark Keightley <mark.keightley@aardman.com>
2021-05-14 10:16:09 +0100

    trying a different approach, starting from a exe and making that in to a service rather than the other way around

---
+++ Attempt2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Configuration.Install;
+
+namespace Attempt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                if (args.Length > 0)
+                {
+                    if (args[0].ToLower() == "install")
+                    {
+                        InstallService();
+                    }
+                    else if (args[0].ToLower() == "uninstall")
+                    {
+                        UninstallService();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Running as exe");
+
+                        Console.WriteLine("Press return to exit");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    TextGenerator.WriteMessage("Running as exe!");
+                }
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new TestService()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        static void InstallService()
+        {
+            ManagedInstallerClass.InstallHelper(
+                new string[] { typeof(Program).Assembly.Location });
+        }
+
+        static void UninstallService()
+        {
+            ManagedInstallerClass.InstallHelper(
+                new string[] { "/u", typeof(Program).Assembly.Location });
+        }
+    }
+}
---
+++ Attempt2/TestService.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceProcess;
+
+namespace Attempt2
+{
+    public class TestService : ServiceBase
+    {
+        public TestService()
+        {
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            TextGenerator.WriteMessage("Running as Service!");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
